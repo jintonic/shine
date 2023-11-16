2d80b38a5c1fe926721a8d5ac53fa4425229a178
bestdeveloper23 <softangel0090@gmail.com>
2023-11-16 08:34:34 +0800

    tub added on left panel

--- js/ModelCategories.js
+++ js/ModelCategories.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { CSG } from './libs/CSGMesh.js';
 
 import { UIDiv, UIPanel, UIRow } from "./libs/ui.js";
 
@@ -54,7 +55,7 @@ function ModelCategory(editor) {
   var direction = mouseScenePosition.sub(camera.position).normalize();
   var distance = -camera.position.y / direction.y;
   var position = camera.position.clone().add(direction.multiplyScalar(distance));
-  
+
   const geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1);
   const mesh = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial());
   mesh.position.copy(position);
@@ -76,12 +77,12 @@ function ModelCategory(editor) {
 
  item.setTextContent(strings.getKey('menubar/add/box'));
  item.dom.setAttribute('draggable', true);
- item.dom.setAttribute('item-type', 'Cone');
+ item.dom.setAttribute('item-type', 'Sphere');
  item.onClick(function () {
 
-		const geometry = new THREE.SphereGeometry( 1, 32, 16, 0, Math.PI * 2, 0, Math.PI );
-		const mesh = new THREE.Mesh( geometry, new THREE.MeshStandardMaterial() );
-		mesh.name = 'Sphere';
+  const geometry = new THREE.SphereGeometry(1, 32, 16, 0, Math.PI * 2, 0, Math.PI);
+  const mesh = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial());
+  mesh.name = 'Sphere';
 
   editor.execute(new AddObjectCommand(editor, mesh));
 
@@ -105,7 +106,7 @@ function ModelCategory(editor) {
   var distance = -camera.position.y / direction.y;
   var position = camera.position.clone().add(direction.multiplyScalar(distance));
 
-  const geometry = new THREE.SphereGeometry( 1, 32, 16, 0, Math.PI * 2, 0, Math.PI );
+  const geometry = new THREE.SphereGeometry(1, 32, 16, 0, Math.PI * 2, 0, Math.PI);
   const mesh = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial());
 
   mesh.position.copy(position);
@@ -127,16 +128,46 @@ function ModelCategory(editor) {
 
  item.setTextContent(strings.getKey('menubar/add/box'));
  item.dom.setAttribute('draggable', true);
- item.dom.setAttribute('item-type', 'Cone');
+ item.dom.setAttribute('item-type', 'eTub');
  item.onClick(function () {
 
   // we need to new each geometry module
-  
-  const geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1);
-  const mesh = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial());
-  mesh.name = 'Box';
 
-  editor.execute(new AddObjectCommand(editor, mesh));
+  var pRMin = 1, pRMax = 1.5, pDz = 2;
+
+  const spheregeometry1 = new THREE.CylinderGeometry(pRMax, pRMax, pDz, 32, 1, false, 0, Math.PI * 2);
+  const spheremesh1 = new THREE.Mesh(spheregeometry1, new THREE.MeshStandardMaterial());
+
+  const spheregeometry2 = new THREE.CylinderGeometry(pRMin, pRMin, pDz, 32, 1, false, 0, Math.PI * 2);
+  const spheremesh2 = new THREE.Mesh(spheregeometry2, new THREE.MeshStandardMaterial());
+
+  const boxgeometry = new THREE.BoxGeometry(pRMax, pDz, pRMax);
+  const boxmesh = new THREE.Mesh(boxgeometry, new THREE.MeshStandardMaterial());
+  boxmesh.geometry.translate(pRMax / 2, 0, pRMax / 2);
+  const MeshCSG1 = CSG.fromMesh(spheremesh1);
+  const MeshCSG2 = CSG.fromMesh(spheremesh2);
+  let MeshCSG3 = CSG.fromMesh(boxmesh);
+
+  let aCSG;
+  aCSG = MeshCSG1.subtract(MeshCSG2);
+  aCSG = aCSG.subtract(MeshCSG3);
+  boxmesh.rotateY(Math.PI / 2);
+  boxmesh.updateMatrix();
+  MeshCSG3 = CSG.fromMesh(boxmesh);
+  aCSG = aCSG.subtract(MeshCSG3);
+  boxmesh.rotateY(Math.PI);
+  boxmesh.updateMatrix();
+  MeshCSG3 = CSG.fromMesh(boxmesh);
+  aCSG = aCSG.subtract(MeshCSG3);
+
+  const finalMesh = CSG.toMesh(aCSG, new THREE.Matrix4());
+  const param = { x: 1, y: 2, z: 1 };
+  finalMesh.geometry.parameters = param;
+  finalMesh.geometry.center();
+  finalMesh.updateMatrix()
+  finalMesh.name = 'Tubs';
+
+  editor.execute(new AddObjectCommand(editor, finalMesh));
 
  });
 
@@ -158,20 +189,49 @@ function ModelCategory(editor) {
   var distance = -camera.position.y / direction.y;
   var position = camera.position.clone().add(direction.multiplyScalar(distance));
 
-  const geometry = new THREE.CylinderGeometry( 0.5, 0.5, 1, 32, 1, false, 0, Math.PI * 2 );
-  const mesh = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial());
+  var pRMin = 1, pRMax = 1.5, pDz = 2;
 
-  mesh.position.copy(position);
-  mesh.name = 'Tubs';
+  const spheregeometry1 = new THREE.CylinderGeometry(pRMax, pRMax, pDz, 32, 1, false, 0, Math.PI * 2);
+  const spheremesh1 = new THREE.Mesh(spheregeometry1, new THREE.MeshStandardMaterial());
 
-  editor.execute(new AddObjectCommand(editor, mesh));
+  const spheregeometry2 = new THREE.CylinderGeometry(pRMin, pRMin, pDz, 32, 1, false, 0, Math.PI * 2);
+  const spheremesh2 = new THREE.Mesh(spheregeometry2, new THREE.MeshStandardMaterial());
+
+  const boxgeometry = new THREE.BoxGeometry(pRMax, pDz, pRMax);
+  const boxmesh = new THREE.Mesh(boxgeometry, new THREE.MeshStandardMaterial());
+  boxmesh.geometry.translate(pRMax / 2, 0, pRMax / 2);
+  const MeshCSG1 = CSG.fromMesh(spheremesh1);
+  const MeshCSG2 = CSG.fromMesh(spheremesh2);
+  let MeshCSG3 = CSG.fromMesh(boxmesh);
+
+  let aCSG;
+  aCSG = MeshCSG1.subtract(MeshCSG2);
+  aCSG = aCSG.subtract(MeshCSG3);
+  boxmesh.rotateY(Math.PI / 2);
+  boxmesh.updateMatrix();
+  MeshCSG3 = CSG.fromMesh(boxmesh);
+  aCSG = aCSG.subtract(MeshCSG3);
+  boxmesh.rotateY(Math.PI);
+  boxmesh.updateMatrix();
+  MeshCSG3 = CSG.fromMesh(boxmesh);
+  aCSG = aCSG.subtract(MeshCSG3);
+
+  const finalMesh = CSG.toMesh(aCSG, new THREE.Matrix4());
+  const param = { x: 1, y: 2, z: 1 };
+  finalMesh.geometry.parameters = param;
+  finalMesh.position.copy(position);
+  finalMesh.geometry.center();
+  finalMesh.updateMatrix()
+  finalMesh.name = 'Tubs';
+
+  editor.execute(new AddObjectCommand(editor, finalMesh));
 
  });
 
  options.add(item);
 
 
- 
+
 
  return container;
 }
