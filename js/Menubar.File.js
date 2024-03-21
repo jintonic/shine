ea38e211164cb0dce6d8aa833cd21997a93906d8
First <109863675+lightfi103@users.noreply.github.com>
2024-03-21 22:11:07 +0800

    rotate add

--- js/Menubar.File.js
+++ js/Menubar.File.js
@@ -113,12 +113,13 @@ function MenubarFile( editor ) {
 
 		console.log(object)
 		var txt = `:volu world BOX 10. 10. 10. G4_AIR\n\n`;
+		txt += `:rotm r000 ${object.rotation.x} ${object.rotation.y} ${object.rotation.z}\n\n`;
 
 		if ( object !== null && object.isMesh != undefined ) {
 			switch (object.name) {
 				case "Box":
 					txt += `:solid box BOX ${object.geometry.parameters.width} ${object.geometry.parameters.depth} ${object.geometry.parameters.height}\n\n`
-					txt += `:volu mybox box ${object.material.name.elementType}\n`
+					txt += `:volu mybox box ${object.material.name.elementType}\n\n`
 					txt += `:place mybox 1 world r000 ${object.position.x} ${object.position.y} ${object.position.z}\n`
 					break;
 				default:
